6ad437dde8f0b8c5bca81150f63e993a9c36d43d
GauriSpears <Gauri@nextmail.ru>
2026-09-16 14:02:06 +0500

    Add files via upload

---
+++ scripts/install-prereq.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+. ./hardupdate
+#New installation or upgrade.
+isupg=${isupg:-true}
+GETPM=${GET:?}
+
+if [ "${GETPM}" == "apt-get" ]; then
+  apt-get update -y
+  apt-get install -y ca-certificates
+  cat > /etc/apt/sources.list.d/gku.list << 'EOF'
+deb https://deb.debian.org/debian experimental main
+deb https://deb.debian.org/debian unstable main
+deb https://deb.debian.org/debian testing main
+EOF
+  sed -i 's/^[[:space:]]*//' /etc/apt/sources.list.d/gku.list
+  cat > /etc/apt/preferences.d/gku.pref << 'EOF'
+Package: *
+Pin: release a=testing
+Pin-Priority: -1
+
+Package: *
+Pin: release a=unstable
+Pin-Priority: -1
+
+Package: *
+Pin: release a=experimental
+Pin-Priority: -1
+EOF
+  sed -i 's/^[[:space:]]*//' /etc/apt/preferences.d/gku.pref
+  apt-get update -y
+  apt-get install -y build-essential autoconf automake libtool git
+  rm -rf /usr/local/ssl/share/man/man1/*.1ossl
+  rm -rf /usr/local/ssl/share/man/man3/*.3ossl
+  rm -rf /usr/local/ssl/share/man/man7/*.7ossl
+  rm -rf /usr/local/ssl/share/doc/openssl
+  rm -rf /usr/local/ssl/bin
+  rm -rf /usr/local/ssl/include
+  rm -rf /usr/local/ssl/lib
+  rm -rf /root/openssl
+  rm -rf /usr/bin/openssl
+  hash -r
+  if ! $isupg; then
+    forcepack "openssl libssl-dev" 2
+  else
+    getpack "openssl libssl-dev" 2
+  fi
+  rm -rf /usr/local/doc/cmake-*
+  rm -rf /usr/local/bin/ccmake
+  rm -rf /usr/local/bin/cmake
+  rm -rf /usr/local/bin/ctest
+  rm -rf /usr/local/bin/cpack
+  rm -rf /usr/local/share/cmake-*
+  rm -rf /usr/local/share/vim/vimfiles/indent/cmake.vim
+  rm -rf /usr/local/share/vim/vimfiles/syntax/cmake.vim
+  rm -rf /usr/local/share/emacs/site-lisp/cmake-mode.el
+  rm -rf /usr/local/share/aclocal/cmake.m4
+  rm -rf /usr/local/share/bash-completion/completions/cmake
+  rm -rf /usr/local/share/bash-completion/completions/cpack
+  rm -rf /usr/local/share/bash-completion/completions/ctest
+  hash -r
+  if ! $isupg; then
+    forcepack cmake 3
+  else
+    getpack cmake 3
+  fi
+elif [ "${GETPM}" == "pacman" ]; then
+  pacman-key --init
+  pacman -Syu --noconfirm
+  pacman -Sy --needed --noconfirm bash grep gcc make autoconf automake libtool git openssl cmake
+elif [ "${GETPM}" == "dnf" ]; then
+  dnf -y update
+  dnf -y install epel-release
+  dnf -y groupinstall "Development Tools"
+  dnf -y update
+  dnf -y install gcc gcc-c++ kernel-devel make git autoconf automake libtool git openssl openssl-devel cmake
+fi
+exit 0
